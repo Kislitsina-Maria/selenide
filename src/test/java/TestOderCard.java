6b2b2909a24256ab490b53c8d12bb5cdc36973c4
Kislitsina-Maria <ma.kis@mail.ru>
2022-10-02 23:08:39 +0300

    First test is ready

--- src/test/java/TestOderCard.java
+++ src/test/java/TestOderCard.java
@@ -1,5 +1,9 @@
+import com.codeborne.selenide.conditions.Visible;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class TestOderCard {
@@ -9,21 +13,15 @@ public class TestOderCard {
 
         open("http://localhost:7777");
         $("[placeholder=\"Город\"]").setValue("Москва");
+        $("[placeholder=\"Дата встречи\"]").setValue("05.11.2022");
+        $("[name=\"name\"]").setValue("Инесса Рюриковна");
+        $("[name=\"phone\"]").setValue("+79340248197");
+        $("[class=\"checkbox__box\"]").click();
+        $("[class=\"button__content\"]").click();
 
-
-
-//        driver.findElement(By.cssSelector("[data-test-id='city']")).sendKeys("Москва");
-//        driver.findElement(By.cssSelector("data-test-id=\"date\"")).sendKeys("05.11.2022");
-//        driver.findElement(By.cssSelector("[data-test-id=\"name\"]")).sendKeys("Инесса Рюриковна");
-//        driver.findElement(By.cssSelector("[data-test-id=\"phone\"]")).sendKeys("+79340248197");
-//        driver.findElement(By.className("checkbox__control")).click();
-//        driver.findElement(By.className("button__content")).click();
-//
-//        String actual = driver.findElement(By.className("notification__icon")).getText().trim().;
-//        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-//
-//        Assertions.assertEquals(expected, actual);
-
-
+        $x("//*[contains(text(),\"Успешно!\")]").should(visible, Duration.ofSeconds(15));
+        $x("//*[contains(text(),\"Встреча успешно забронирована на\")]");
+        $x("//*[contains(text(),\"05.11.2022\")]");
     }
+
 }
